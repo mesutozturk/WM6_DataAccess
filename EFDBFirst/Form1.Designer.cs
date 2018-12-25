a34c956e5a44d4423c6b9db623e9883850e5f95a
Mesut Öztürk <mesutoztrk@msn.com>
2018-12-25 10:24:59 +0300

    linq part1

--- EFDBFirst/Form1.Designer.cs
+++ EFDBFirst/Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvTest = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvTest
+            // 
+            this.dgvTest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTest.Location = new System.Drawing.Point(13, 13);
+            this.dgvTest.Name = "dgvTest";
+            this.dgvTest.Size = new System.Drawing.Size(954, 449);
+            this.dgvTest.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(979, 474);
+            this.Controls.Add(this.dgvTest);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvTest;
     }
 }
 
--- EFDBFirst/Form1.cs
+++ EFDBFirst/Form1.cs
@@ -16,5 +16,77 @@ namespace EFDBFirst
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // tum urunleri listeleyelim
+            NorthwindEntities db = new NorthwindEntities();
+            // product uzerinden ilerlendigi icin tum productlar gozukmeli yani left join
+            var sorgu1 = db.Products
+                .Select(x => new
+                {
+                    x.ProductName,
+                    x.UnitPrice,
+                    x.Category.CategoryName
+                })
+                .ToList();
+            //burada iliski kuruldugu icin iliskideki kurala gore inner join calisir.
+            var sorgu2 = from p in db.Products
+                         join cat in db.Categories on p.CategoryID equals cat.CategoryID
+                         select new
+                         {
+                             UrunAdi = p.ProductName,
+                             Fiyat = p.UnitPrice,
+                             Kategori = cat.CategoryName
+                         };
+
+            dgvTest.DataSource = sorgu2.ToList();
+
+            //calisanlarimi email adresleri ile listeleyin
+
+            var sorgu3 = db.Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    Email = (x.FirstName.Substring(0, 1) + x.LastName + "@northwind.com").ToLower()
+                }).ToList();
+            dgvTest.DataSource = sorgu3;
+            var sorgu4 = from emp in db.Employees
+                         select new
+                         {
+                             emp.FirstName,
+                             emp.LastName,
+                             Email = (emp.FirstName.Substring(0, 1) + emp.LastName + "@northwind.com").ToLower()
+                         };
+
+            dgvTest.DataSource = sorgu4.ToList();
+
+            this.Text = $"{db.Products.Average(x => x.UnitPrice):c2}";
+
+            var sorgu5 = db.Products
+                .Where(x => x.UnitPrice >= db.Products.Average(y => y.UnitPrice))
+                .Select(x => new
+                {
+                    x.ProductName,
+                    Fiyat = x.UnitPrice,
+                    x.Category.CategoryName
+                })
+                .OrderByDescending(x => x.Fiyat)
+                .ToList();
+            dgvTest.DataSource = sorgu5;
+
+            var sorgu6 = from p in db.Products
+                where p.UnitPrice >= db.Products.Average(x => x.UnitPrice)
+                orderby p.UnitPrice descending
+                select new
+                {
+                    p.ProductName,
+                    Fiyat = p.UnitPrice,
+                    p.Category.CategoryName
+                };
+
+            dgvTest.DataSource = sorgu6.ToList();
+        }
     }
 }
