6e542a0719f46d913a2def2801ef8b4401cb9606
Mesut Öztürk <mesutoztrk@msn.com>
2018-12-25 09:18:17 +0300

    db model ok

--- EFDBFirst/Form1.Designer.cs
+++ EFDBFirst/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(660, 421);
+            this.ClientSize = new System.Drawing.Size(778, 392);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
